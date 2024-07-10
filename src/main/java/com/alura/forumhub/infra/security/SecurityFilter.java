8296ece0befcb34e8f9867124bf1f2264835b9a8
dyonne <bydyonne@gmail.com>
2024-07-09 22:09:12 -0300

    Security filter class configuration

---
+++ src/main/java/com/alura/forumhub/infra/security/SecurityFilter.java
@@ -0,0 +1,45 @@
+package com.alura.forumhub.infra.security;
+
+import com.alura.forumhub.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        var token = this.recoverToken(request);
+        if (token != null) {
+            var login = tokenService.validateToken(token);
+            UserDetails user = userRepository.findByLogin(login);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()); // make verification
+            SecurityContextHolder.getContext().setAuthentication(authentication); // saves the user in the authentication context
+        }
+        filterChain.doFilter(request, response); // call the next filter
+    }
+
+    // method responsible for obtaining the Authorization header and retrieving only the Token value
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer", "").trim();
+    }
+}
